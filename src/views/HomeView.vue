ef2103a163482a60483161664d890ba59cce8d0a
Krutzzio <39598943+Krutzzio@users.noreply.github.com>
2023-04-24 08:17:16 +0200

    modified:   src/views/HomeView.vue 	modified:   src/App.vue     modified:   src/components/roomComponents/RoomComponent.css 	modified:   src/components/roomComponents/RoomComponent.js 	modified:   src/views/RoomView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,25 @@
 <template>
+  <div class="menu">
+    <router-link to="/room"  v-if="$route.path != '/room'" >Començar</router-link>
+  </div>
+  
 </template>
   
-  <script>
+<script>
  
-  </script>
-  
-  <style>
+</script>
 
-  </style>
+<style>
+  .menu{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    top: 50%;
+    left: 50%;
+    outline: solid 1px black;
+    
+    height: 600px;
+    width: 800px;
+  }
+</style>
   
